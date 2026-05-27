d22c120a7647c212d19c4a829591fe54cda4b1e4
Dastan_27 <maikleo2019@gmail.com>
2026-05-27 13:08:39 -0500

    Ejercicios solucionados

---
+++ 07-16meduum3SumCloset.ts
@@ -0,0 +1,34 @@
+function threeSumClosest(nums: number[], target: number): number {
+    nums.sort((a, b) => a - b);
+
+    let closestSum = nums[0] + nums[1] + nums[2];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const currentSum = nums[i] + nums[left] + nums[right];
+
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+
+            if (currentSum === target) {
+                return currentSum; // Retornamos inmediatamente
+            } 
+            else if (currentSum < target) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+}
+
+
+console.log(threeSumClosest([-1, 2, 1, -4], 1)); 
+console.log(threeSumClosest([0, 0, 0], 1)); 
---
+++ 08-17mediumLetterCombinationsOfAPhoneNumber.ts
@@ -0,0 +1,39 @@
+function letterCombinations(digits: string): string[] {
+    if (digits.length === 0) return [];
+
+    const phoneMap: { [key: string]: string } = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+
+    const result: string[] = [];
+
+    const backtrack = (index: number, currentStr: string) => {
+
+        if (index === digits.length) {
+            result.push(currentStr);
+            return;
+        }
+
+        const currentDigit = digits[index];
+        const letters = phoneMap[currentDigit];
+
+        for (let i = 0; i < letters.length; i++) {
+            backtrack(index + 1, currentStr + letters[i]);
+        }
+    };
+
+    backtrack(0, "");
+
+    return result;
+}
+
+
+console.log(letterCombinations("23")); 
+console.log(letterCombinations("2")); 
