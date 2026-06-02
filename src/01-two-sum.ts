59d51bf078b4a9f25a59c79459c6d414555860fe
fqrid <faridzemanate@gmail.com>
2026-06-02 13:17:58 -0500

    Solución de errores de compilación y optimización de tsconfig

--- src/01-two-sum.ts
+++ src/01-two-sum.ts
@@ -3,7 +3,7 @@ export function twoSum(nums: number[], target: number): number[] {
   const seen = new Map<number, number>();
 
   for (let index = 0; index < nums.length; index += 1) {
-    const current = nums[index];
+    const current = nums[index]!;
     const complement = target - current;
     const match = seen.get(complement);
 
